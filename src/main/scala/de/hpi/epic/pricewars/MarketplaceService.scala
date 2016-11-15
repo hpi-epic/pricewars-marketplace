ce06d802c4a728eb7921293b34c77d6456be4d35
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-11-15 13:20:51 +0100

    Add CORSSupport

--- src/main/scala/de/hpi/epic/pricewars/MarketplaceService.scala
+++ src/main/scala/de/hpi/epic/pricewars/MarketplaceService.scala
@@ -16,141 +16,142 @@ class MarketplaceServiceActor extends Actor with MarketplaceService {
 /**
   * Created by Jan on 01.11.2016.
   */
-trait MarketplaceService extends HttpService {
+trait MarketplaceService extends HttpService with CORSSupport {
   val route = respondWithMediaType(MediaTypes.`application/json`) {
-    path("offers") {
-      get {
-        parameter('product_id.as[Long]?) { product_id =>
-          complete {
-            DatabaseStore.getOffers(product_id)
-          }
-        }
-      } ~
-        post {
-          entity(as[Offer]) { offer =>
-            detach() {
-              complete {
-                DatabaseStore.addOffer(offer).successHttpCode(StatusCodes.Created)
-              }
-            }
-          }
-        }
-    } ~
-      path("offers" / LongNumber) { id =>
+    cors {
+      path("offers") {
         get {
-          complete {
-            DatabaseStore.getOffer(id)
+          parameter('product_id.as[Long] ?) { product_id =>
+            complete {
+              DatabaseStore.getOffers(product_id)
+            }
           }
         } ~
-          delete {
-            complete {
-              val res = DatabaseStore.deleteOffer(id)
-              res match {
-                case Success(v) => StatusCodes.NoContent
-                case f: Failure[Unit] => StatusCode.int2StatusCode(f.code) -> f.toJson.toString()
-              }
-            }
-          } ~
-          put {
+          post {
             entity(as[Offer]) { offer =>
               detach() {
                 complete {
-                  DatabaseStore.updateOffer(id, offer)
+                  DatabaseStore.addOffer(offer).successHttpCode(StatusCodes.Created)
                 }
               }
             }
           }
       } ~
-      path("offers" / LongNumber / "buy") { id =>
-        post {
-          entity(as[BuyRequest]) { buyRequest =>
-            detach() {
+        path("offers" / LongNumber) { id =>
+          get {
+            complete {
+              DatabaseStore.getOffer(id)
+            }
+          } ~
+            delete {
               complete {
-                DatabaseStore.getOffer(id).flatMap(offer => DatabaseStore.getMerchant(offer.merchant_id.toLong)) match {
-                  case Success(merchant) => MerchantConnector.notifyMerchant(merchant, id, buyRequest.amount, buyRequest.price)
+                val res = DatabaseStore.deleteOffer(id)
+                res match {
+                  case Success(v) => StatusCodes.NoContent
+                  case f: Failure[Unit] => StatusCode.int2StatusCode(f.code) -> f.toJson.toString()
                 }
-                DatabaseStore.buyOffer(id, buyRequest.price, buyRequest.amount).successHttpCode(StatusCodes.NoContent)
+              }
+            } ~
+            put {
+              entity(as[Offer]) { offer =>
+                detach() {
+                  complete {
+                    DatabaseStore.updateOffer(id, offer)
+                  }
+                }
               }
             }
-          }
-        }
-      } ~
-      path("offers" / LongNumber / "restock") { id =>
-        patch {
-          entity(as[OfferPatch]) { offer =>
-            complete {
-              offer.amount match {
-                case Some(amount) => DatabaseStore.restockOffer(id, amount)
-                case _ => StatusCodes.InternalServerError -> "no amount specified"
-              }
-            }
-          }
-        }
-      } ~
-      path("merchants") {
-        get {
-          complete {
-            DatabaseStore.getMerchants
-          }
         } ~
+        path("offers" / LongNumber / "buy") { id =>
           post {
-            entity(as[Merchant]) { merchant =>
+            entity(as[BuyRequest]) { buyRequest =>
               detach() {
                 complete {
-                  DatabaseStore.addMerchant(merchant).successHttpCode(StatusCodes.Created)
+                  DatabaseStore.getOffer(id).flatMap(offer => DatabaseStore.getMerchant(offer.merchant_id.toLong)) match {
+                    case Success(merchant) => MerchantConnector.notifyMerchant(merchant, id, buyRequest.amount, buyRequest.price)
+                  }
+                  DatabaseStore.buyOffer(id, buyRequest.price, buyRequest.amount).successHttpCode(StatusCodes.NoContent)
                 }
               }
             }
           }
-      } ~
-      path("merchants" / LongNumber) { id =>
-        get {
-          complete {
-            DatabaseStore.getMerchant(id)
-          }
         } ~
-          delete {
-            complete {
-              val res = DatabaseStore.deleteMerchant(id)
-              res match {
-                case Success(v) => StatusCodes.NoContent
-                case f: Failure[Unit] => StatusCode.int2StatusCode(f.code) -> f.toJson.toString()
-              }
-            }
-          }
-      } ~
-      path("consumers") {
-        get {
-          complete {
-            DatabaseStore.getConsumers
-          }
-        } ~
-          post {
-            entity(as[Consumer]) { consumer =>
-              detach() {
-                complete {
-                  DatabaseStore.addConsumer(consumer).successHttpCode(StatusCodes.Created)
+        path("offers" / LongNumber / "restock") { id =>
+          patch {
+            entity(as[OfferPatch]) { offer =>
+              complete {
+                offer.amount match {
+                  case Some(amount) => DatabaseStore.restockOffer(id, amount)
+                  case _ => StatusCodes.InternalServerError -> "no amount specified"
                 }
               }
             }
           }
-      } ~
-      path("consumers" / LongNumber) { id =>
-        get {
-          complete {
-            DatabaseStore.getConsumer(id)
-          }
         } ~
-          delete {
+        path("merchants") {
+          get {
             complete {
-              val res = DatabaseStore.deleteConsumer(id)
-              res match {
-                case Success(v) => StatusCodes.NoContent
-                case f: Failure[Unit] => StatusCode.int2StatusCode(f.code) -> f.toJson.toString()
+              DatabaseStore.getMerchants
+            }
+          } ~
+            post {
+              entity(as[Merchant]) { merchant =>
+                detach() {
+                  complete {
+                    DatabaseStore.addMerchant(merchant).successHttpCode(StatusCodes.Created)
+                  }
+                }
               }
             }
-          }
-      }
+        } ~
+        path("merchants" / LongNumber) { id =>
+          get {
+            complete {
+              DatabaseStore.getMerchant(id)
+            }
+          } ~
+            delete {
+              complete {
+                val res = DatabaseStore.deleteMerchant(id)
+                res match {
+                  case Success(v) => StatusCodes.NoContent
+                  case f: Failure[Unit] => StatusCode.int2StatusCode(f.code) -> f.toJson.toString()
+                }
+              }
+            }
+        } ~
+        path("consumers") {
+          get {
+            complete {
+              DatabaseStore.getConsumers
+            }
+          } ~
+            post {
+              entity(as[Consumer]) { consumer =>
+                detach() {
+                  complete {
+                    DatabaseStore.addConsumer(consumer).successHttpCode(StatusCodes.Created)
+                  }
+                }
+              }
+            }
+        } ~
+        path("consumers" / LongNumber) { id =>
+          get {
+            complete {
+              DatabaseStore.getConsumer(id)
+            }
+          } ~
+            delete {
+              complete {
+                val res = DatabaseStore.deleteConsumer(id)
+                res match {
+                  case Success(v) => StatusCodes.NoContent
+                  case f: Failure[Unit] => StatusCode.int2StatusCode(f.code) -> f.toJson.toString()
+                }
+              }
+            }
+        }
+    }
   }
-
 }
