78fabf60ef3e7cd85b4a4169ab1d6c752242b2ac
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-01-04 19:15:01 +0100

    Change consumer to use a string for id and token

--- src/main/scala/de/hpi/epic/pricewars/Consumer.scala
+++ src/main/scala/de/hpi/epic/pricewars/Consumer.scala
@@ -5,10 +5,11 @@ import scalikejdbc._
 case class Consumer(api_endpoint_url: String,
                     consumer_name: String,
                     description: String,
-                    consumer_id: Option[Long] )
+                    consumer_id: Option[String],
+                    consumer_token: Option[String])
 
 object Consumer extends SQLSyntaxSupport[Consumer] {
   override val tableName = "consumers"
   def apply(rs: WrappedResultSet) = new Consumer(
-    rs.string("api_endpoint_url"), rs.string("consumer_name"), rs.string("description"), Some(rs.long("consumer_id")))
+    rs.string("api_endpoint_url"), rs.string("consumer_name"), rs.string("description"), rs.stringOpt("consumer_id"), rs.stringOpt("consumer_token"))
 }
