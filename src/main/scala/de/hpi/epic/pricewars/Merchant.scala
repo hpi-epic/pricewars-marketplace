7aeaf4d4ee9595b05d07f85dc4963f6180aa87d2
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-01-04 19:15:46 +0100

    Change merchant to use a string for id and token

--- src/main/scala/de/hpi/epic/pricewars/Merchant.scala
+++ src/main/scala/de/hpi/epic/pricewars/Merchant.scala
@@ -5,10 +5,11 @@ import scalikejdbc._
 case class Merchant( api_endpoint_url: String,
                      merchant_name: String,
                      algorithm_name: String,
-                     merchant_id: Option[Long] )
+                     merchant_id: Option[String],
+                     merchant_token: Option[String])
 
 object Merchant extends SQLSyntaxSupport[Merchant] {
   override val tableName = "merchants"
   def apply(rs: WrappedResultSet) = new Merchant(
-    rs.string("api_endpoint_url"), rs.string("merchant_name"), rs.string("algorithm_name"), Some(rs.long("merchant_id")))
+    rs.string("api_endpoint_url"), rs.string("merchant_name"), rs.string("algorithm_name"), rs.stringOpt("merchant_id"), rs.stringOpt("merchant_token"))
 }
