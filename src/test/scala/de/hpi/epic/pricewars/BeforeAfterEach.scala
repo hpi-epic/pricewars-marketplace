e13e81c4feced6a3be042fe912febbba2f0444bc
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-01-04 19:18:13 +0100

    Remove created-by-Comment

--- src/test/scala/de/hpi/epic/pricewars/BeforeAfterEach.scala
+++ src/test/scala/de/hpi/epic/pricewars/BeforeAfterEach.scala
@@ -1,8 +1,5 @@
 package de.hpi.epic.pricewars
 
-/**
-  * Created by sebastian on 09.11.16
-  */
 trait BeforeAfterEach extends org.specs2.specification.BeforeAfterEach {
 
   protected def before: Any = {
